513ea62a5a8230832f5703f4a94c1b2468723fd3
Jimmy Lam <jimmylam@Jimmys-MacBook-Pro.local>
2020-01-15 22:06:24 -0800

    names now can be updated

--- GUI.py
+++ GUI.py
@@ -6,8 +6,18 @@ Last Modified: 1/13/20
 """
 
 import tkinter as tk
+import time
 
-def guiWindow():
+def printGUI(textWindow, text, hStartIdx, hEndIdx):
+    textWindow.pack()
+    textWindow.insert('1.0', ' ' * 60)  # clear names
+    textWindow.insert('1.0', text)
+
+    textWindow.tag_add('tag1', '1.{}'.format(hStartIdx), '1.{}'.format(hEndIdx))
+    textWindow.tag_config('tag1', background='green')
+
+
+def main():
     name1 = "Maura McCabe"
     name2 = "Jimmy Lam"
     name3 = "Lucas Hyatt"
@@ -17,24 +27,40 @@ def guiWindow():
     win = tk.Tk()
     win.title("Current Cold Calling List")
 
-    t = tk.Text(win, height=1, width=60, font=('Courier', 16))
-    t.pack()
-    #t.insert('1.0', ' ' * 60)  # cannot use \t, won't work
-    t.insert(tk.END, name1)  # insert('<line#>.<index>', <text to insert>)
-    t.insert(tk.END, '   ')
-    t.insert(tk.END, name2)  # offset by 15 characters from beginning per name
-    t.insert(tk.END, '   ')
-    t.insert(tk.END, name3)
-    t.insert(tk.END, '   ')
-    t.insert(tk.END, name4)
+    textWindow = tk.Text(win, height=1, width=60, font=('Courier', 16))
 
-    beginIdx = len(name1) + 3
-    t.tag_add('tag1', '1.{}'.format(beginIdx), '1.{}'.format(beginIdx + len(name2)))
-    t.tag_config('tag1', background='green')
+    names = "{}   {}   {}   {}".format(name1, name2, name3, name4)
 
-    tk.mainloop()
+    highlightBegin = len(name1) + 3
+    highlightEnd = highlightBegin + len(name2)
+    printGUI(textWindow, names, highlightBegin, highlightEnd)
+    win.update()
 
-guiWindow()
+    print('highlighting changing in 3 seconds...')
+    time.sleep(3)
+
+    highlightBegin = len(name1) + len(name2) + 6
+    highlightEnd = highlightBegin + len(name3)
+    printGUI(textWindow, names, highlightBegin, highlightEnd)
+    win.update()
+
+    print('list changing in 3 seconds...')
+    time.sleep(3)
+
+    names = "{}   {}   {}   {}".format(name2, name3, name4, name5)
+
+    highlightBegin = 0
+    highlightEnd = highlightBegin + len(name2)
+    printGUI(textWindow, names, highlightBegin, highlightEnd)
+    win.update()
+
+    print("\n--- End of test. Close the cold calling window to exit ---")
+
+    win.mainloop() # blocks until the window is closed
+
+
+if __name__ == '__main__':
+    main()
 
 """
 Sources:
