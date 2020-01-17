2a1120474d9bf965c80c7405b6aec8800d645a4a
Jimmy Lam <jimmylam@Jimmys-MacBook-Pro.local>
2020-01-16 20:59:32 -0800

    added support for arrow key input

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@
 The main GIU window for selecting students
 
 Author: Jimmy Lam
-Last Modified: 1/13/20
+Last Modified: 1/16/20
 """
 
 import tkinter as tk
@@ -11,25 +11,39 @@ import time
 class GUI:
     def __init__(self, winTitle: str, highlightColor: str):
         self.title = winTitle
-        self.t = tk.Tk()
+        self.mainWindow = tk.Tk()
         self.bgColor = highlightColor
-        self.window = tk.Text(self.t, height=1, width=60, font=('Courier', 16))
+        self.text = tk.Text(self.mainWindow, height=1, width=60, font=('Courier', 16))
 
-        self.t.title(self.title)  # set window description
+        self.mainWindow.title(self.title)  # set window title (grey bar at top)
 
-    def update(self, text: str, highlightStart: int, highlightEnd: int):
+    def leftKey(self, event):
+        print("Left key pressed")
+
+    def rightKey(self, event):
+        print("Right key pressed")
+
+    def upKey(self, event):
+        print("Up key pressed")
+
+    def downKey(self, event):
+        print("Down key pressed")
+
+    def update(self, inText: str, highlightStart: int, highlightEnd: int):
         """ Prints the names given in <text> to the GUI screen.
         highlightStart is the starting index of the highlighting
         and highlightEnd is the ending index.
         """
-        self.window.pack()
-        self.window.insert('1.0', ' ' * 60)  # clear names
-        self.window.insert('1.0', text)      # write text to GUI
+        self.text.pack()
+        self.text.configure(state='normal')  # reset state in order to change the names
+        self.text.insert('1.0', ' ' * 60)    # clear names
+        self.text.insert('1.0', inText)      # write text to GUI
 
         # now add highlighting
-        self.window.tag_add('tag1', '1.{}'.format(highlightStart), '1.{}'.format(highlightEnd))
-        self.window.tag_config('tag1', background=self.bgColor)
-        self.t.update()
+        self.text.tag_add('tag1', '1.{}'.format(highlightStart), '1.{}'.format(highlightEnd))
+        self.text.tag_config('tag1', background=self.bgColor)
+        self.text.configure(state='disabled')  # prevents user from clicking and editing the text
+        self.mainWindow.update()
 
 def main():
     name1 = "Maura McCabe"
@@ -40,7 +54,7 @@ def main():
 
     gui = GUI('Students on deck', 'green')
 
-    print('Starting GUI test')
+    print('--- Starting GUI test ---')
 
     names = "{}   {}   {}   {}".format(name1, name2, name3, name4)
 
@@ -48,25 +62,18 @@ def main():
     highlightEnd = highlightBegin + len(name2)
     gui.update(names, highlightBegin, highlightEnd)
 
-    print('highlighting changing in 3 seconds...')
-    time.sleep(3)
+    # support for arrow key presses, bind() takes in function to use like pthread_create()
 
-    highlightBegin = len(name1) + len(name2) + 6
-    highlightEnd = highlightBegin + len(name3)
-    gui.update(names, highlightBegin, highlightEnd)
+    gui.mainWindow.bind("<Left>", gui.leftKey)
+    gui.mainWindow.bind("<Right>", gui.rightKey)
+    gui.mainWindow.bind("<Up>", gui.upKey)
+    gui.mainWindow.bind("<Down>", gui.downKey)
 
-    print('list changing in 3 seconds...')
-    time.sleep(3)
+    print("\033[38;5;220mClick on the cold call window. After pressing an arrow key,",
+    "\na message should be displayed. Close the cold call window to end the program.",
+    "\nNote: the names and highlighting should not update for this test.\033[0m")
 
-    names = "{}   {}   {}   {}".format(name2, name3, name4, name5)
-
-    highlightBegin = 0
-    highlightEnd = highlightBegin + len(name2)
-    gui.update(names, highlightBegin, highlightEnd)
-
-    print("\n--- End of test. Close the cold calling window to exit ---")
-
-    gui.t.mainloop()  # blocks until the window is closed
+    gui.mainWindow.mainloop()  # blocks until the window is closed
 
 if __name__ == '__main__':
     main()
@@ -76,5 +83,7 @@ Sources:
 - examples: https://www.python-course.eu/tkinter_text_widget.php
 - font size edit: https://stackoverflow.com/questions/30685308/how-do-i-change-the-text-size-in-a-label-widget-python-tkinter
 - highlight name: https://www.tutorialspoint.com/python/tk_text.htm
+- read-only window: https://stackoverflow.com/questions/3842155/is-there-a-way-to-make-the-tkinter-text-widget-read-only
+- key input: https://stackoverflow.com/questions/19895877/tkinter-cant-bind-arrow-key-events
 """
 
